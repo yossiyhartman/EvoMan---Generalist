0108a8780ee551c04884cebe92de3853ffc79004
Yossi Hartman <yossiyhartman@gmail.com>
2024-10-07 14:54:05 +0200

    moved print function to class

--- classes/Logger.py
+++ classes/Logger.py
@@ -2,6 +2,7 @@ class Logger:
     def __init__(self, headers) -> None:
         self.headers = headers
         self.lengths = [len(header) for header in headers]
+        self.logs = {h: [] for h in headers}
 
     def print_headers(self):
         header_row = " | ".join(str(header).ljust(length) for length, header in zip(self.lengths, self.headers))
@@ -10,3 +11,10 @@ class Logger:
 
     def print_log(self, log):
         print(" | ".join(str(value).ljust(length) for length, value in zip(self.lengths, log)))
+
+    def save_log(self, log):
+        # print the logs
+        self.print_log(log.values())
+
+        for k, v in log.items():
+            self.logs[k].append(v)
