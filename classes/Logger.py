c71a0eaa0e8ff394e10a568c8771cbbedb03a2c5
Yossi Hartman <yossiyhartman@gmail.com>
2024-10-10 19:00:24 +0200

    add print toggle button

--- classes/Logger.py
+++ classes/Logger.py
@@ -1,20 +1,23 @@
 class Logger:
-    def __init__(self, headers) -> None:
+    def __init__(self, headers, print=True) -> None:
         self.headers = headers
         self.lengths = [len(header) for header in headers]
         self.logs = {h: [] for h in headers}
+        self.print = print
 
     def print_headers(self):
-        header_row = " | ".join(str(header).ljust(length) for length, header in zip(self.lengths, self.headers))
-        print("\n" + header_row)
-        print("-" * len(header_row))
+        if self.print:
+            header_row = " | ".join(str(header).ljust(length) for length, header in zip(self.lengths, self.headers))
+            print("\n" + header_row)
+            print("-" * len(header_row))
 
     def print_log(self, log):
         print(" | ".join(str(value).ljust(length) for length, value in zip(self.lengths, log)))
 
     def save_log(self, log):
         # print the logs
-        self.print_log(log.values())
+        if self.print:
+            self.print_log(log.values())
 
         for k, v in log.items():
             self.logs[k].append(v)
